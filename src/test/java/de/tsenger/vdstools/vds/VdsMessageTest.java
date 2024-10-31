5695c1b94684f3ad15e87d2fbfc8fd26eaa32b9b
Tobias Senger <tobias@t-senger.de>
2024-10-31 08:59:55 +0100

    updated SealCodings, added test cases

--- src/test/java/de/tsenger/vdstools/vds/VdsMessageTest.java
+++ src/test/java/de/tsenger/vdstools/vds/VdsMessageTest.java
@@ -1,7 +1,9 @@
 package de.tsenger.vdstools.vds;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import org.bouncycastle.util.Arrays;
 import org.bouncycastle.util.encoders.Hex;
 import org.junit.Test;
 
@@ -19,6 +21,13 @@ public class VdsMessageTest {
 		assertEquals(
 				"02305cba135875976ec066d417b59e8c6abc133c133c133c133c3fef3a2938ee43f1593d1ae52dbb26751fe64b7c133c136b0306d79519a65306",
 				Hex.toHexString(vdsMessage.getEncoded()));
+	}
+	
+	@Test
+	public void testFromByteArray() {
+		byte[] messageBytes = Hex.decode("02305cba135875976ec066d417b59e8c6abc133c133c133c133c3fef3a2938ee43f1593d1ae52dbb26751fe64b7c133c136b0306d79519a65306");
+		VdsMessage message = VdsMessage.fromByteArray(messageBytes, "RESIDENCE_PERMIT");
+		assertTrue(Arrays.areEqual(messageBytes, message.getEncoded()));
 	}
 
 }
--- src/test/java/de/tsenger/vdstools/vds/VdsSignatureTest.java
+++ src/test/java/de/tsenger/vdstools/vds/VdsSignatureTest.java
@@ -1,6 +1,12 @@
 package de.tsenger.vdstools.vds;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
 import java.security.KeyStore;
+
+import org.bouncycastle.util.encoders.Hex;
+import org.junit.Test;
 
 public class VdsSignatureTest {
 
@@ -8,4 +14,35 @@ public class VdsSignatureTest {
 	static String keyStoreFile = "src/test/resources/vdstools_testcerts.bks";
 	static KeyStore keystore;
 
+	static final byte[] plainSignature = Hex.decode(
+			"3c8b104fd4a8ad11157f87dadd05407f0cefa3ad0155c1179765933089896357e1b6fdbb3b2b003d6ee34875d6db833e05fffe9d99378eb01ae988c638c2eb27");
+
+	@Test
+	public void testGetPlainSignatureBytes() {
+		VdsSignature signature = new VdsSignature(plainSignature);
+		assertEquals(
+				"3c8b104fd4a8ad11157f87dadd05407f0cefa3ad0155c1179765933089896357e1b6fdbb3b2b003d6ee34875d6db833e05fffe9d99378eb01ae988c638c2eb27",
+				Hex.toHexString(signature.getPlainSignatureBytes()));
+	}
+
+	@Test
+	public void testGetDerSignatureBytes() {
+		VdsSignature signature = new VdsSignature(plainSignature);
+		assertEquals(
+				"304502203c8b104fd4a8ad11157f87dadd05407f0cefa3ad0155c1179765933089896357022100e1b6fdbb3b2b003d6ee34875d6db833e05fffe9d99378eb01ae988c638c2eb27",
+				Hex.toHexString(signature.getDerSignatureBytes()));
+	}
+
+	@Test
+	public void testFromByteArray() throws IOException {
+		byte[] vdsSigBytes = new byte[] { (byte) 0xff, 6, 1, 2, 3, 4, 5, 6 };
+		VdsSignature signature = VdsSignature.fromByteArray(vdsSigBytes);
+		assertEquals("010203040506", Hex.toHexString(signature.getPlainSignatureBytes()));
+		assertEquals("300a02030102030203040506", Hex.toHexString(signature.getDerSignatureBytes()));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testFromByteArray_IllegalArgumentException() throws IOException {
+		VdsSignature.fromByteArray(plainSignature);
+	}
 }
