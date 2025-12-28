fc778d6d69488950d88e097f8d868f289975dab0
Tobias Senger <tobias@t-senger.de>
2025-12-28 16:33:07 +0100

    prepare v0.10.3_SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "de.tsenger"
-version = "0.10.2"
+version = "0.10.3_SNAPSHOT"
 description = "Kotlin multiplatform library to encode/sign and decode/verify Visible Digital Seals"
 
 repositories {
