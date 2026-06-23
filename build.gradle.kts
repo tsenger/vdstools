f678a8e31246eb984e4ae5a1c112450773fd9146
Tobias Senger <tobias@t-senger.de>
2026-06-23 16:26:41 +0200

    build: generate KDoc javadoc jar via Dokka for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.maven.publish)
+    alias(libs.plugins.dokka)
 }
 
 group = "de.tsenger"
